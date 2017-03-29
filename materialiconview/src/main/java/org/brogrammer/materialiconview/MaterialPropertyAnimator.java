43eaecac99efe0cc746b8c1ceb70ac8c35e32902
Mitch Miller <socmodder@gmail.com>
2017-03-29 17:32:52 -0400

    fix issue with api 15

--- materialiconview/src/main/java/org/brogrammer/materialiconview/MaterialPropertyAnimator.java
+++ materialiconview/src/main/java/org/brogrammer/materialiconview/MaterialPropertyAnimator.java
@@ -12,6 +12,7 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
+import android.support.v4.view.ViewCompat;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -436,7 +437,7 @@ public class MaterialPropertyAnimator {
      */
     private void launchAnimation() {
         materialIconView.removeCallbacks(animationStarter);
-        materialIconView.postOnAnimation(animationStarter);
+        ViewCompat.postOnAnimation(materialIconView, animationStarter);
     }
 
     /**
